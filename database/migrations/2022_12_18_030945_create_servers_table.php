f43ab525d5a996b948f5e9b69df5388b2c74066d
negrahodzic <negra.hodzic@gmail.com>
2022-12-18 04:36:28 +0100

    Added 3 migrations

---
+++ database/migrations/2022_12_18_030945_create_servers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servers', function (Blueprint $table) {
+            $table->id();
+            $table->string('server_id');
+            $table->boolean('enabled_bot')->default(0);
+            $table->string('role_admin_id');
+            $table->string('role_results_id');
+            $table->string('channel_logs_id');
+            $table->string('reaction_correct_id')->default('1037825536782770206');
+            $table->string('reaction_incorrect_id')->default('1037825532651372554');
+            $table->string('reaction_waitlist_id')->default('1037825525705609256');
+            $table->string('reaction_banned_id')->default('1037825529866366986');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servers');
+    }
+};
---
+++ database/migrations/2022_12_18_031354_create_slots_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slots', function (Blueprint $table) {
+            $table->id();
+            $table->integer('slot_number');
+            $table->boolean('is_vip')->default(0);
+            $table->string('team_name');
+            $table->string('team_tag');
+            $table->string('team_manager');
+            $table->enum('choices', array('confirmed', 'canceled', ""))->default("");
+
+            $table->foreignId('scrim_id')
+                ->constrained('scrims')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slots');
+    }
+};
