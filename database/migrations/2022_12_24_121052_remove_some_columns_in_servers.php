f4ccdd797d01406e5dfa0a3e31163f7948f6db1f
negrahodzic <negra.hodzic@gmail.com>
2022-12-24 14:49:36 +0100

    Made migrations: remove and change/rename

---
+++ database/migrations/2022_12_24_121052_remove_some_columns_in_servers.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('servers', function (Blueprint $table) {
+            $table->dropColumn('reaction_correct_id');
+            $table->dropColumn('reaction_incorrect_id');
+            $table->dropColumn('reaction_waitlist_id');
+            $table->dropColumn('reaction_banned_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('servers', function (Blueprint $table) {
+            //
+        });
+    }
+};
